98c3a0ec645b71bcc58dc2e189e6a142775e5e68
tk-08dfd <nguyenphuyenkhang@icloud.com>
2026-03-22 19:58:19 +0700

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-22T12:57:51.191Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-22T12:58:18.081Z
+Commit: Resolve null pointer exception in user service
