ee9c80d9840c3069bbc10b532a25d1459c71c814
tk-08dfd <nguyenphuyenkhang@icloud.com>
2026-03-22 19:58:32 +0700

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-22T12:57:47.436Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-22T12:58:31.558Z
+Commit: Fix broken links in documentation
