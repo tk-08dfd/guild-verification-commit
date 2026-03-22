3a0e757e3bf752ac1fa4022df09fca726fc33bc4
tk-08dfd <nguyenphuyenkhang@icloud.com>
2026-03-22 19:58:59 +0700

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-22T12:58:53.596Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-22T12:58:59.357Z
+Commit: Fix broken links in documentation
