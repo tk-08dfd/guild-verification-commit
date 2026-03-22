9110ad788b794d9863e65d2c4afc370121677143
tk-08dfd <nguyenphuyenkhang@icloud.com>
2026-03-22 19:58:25 +0700

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-22T12:58:18.081Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-22T12:58:24.842Z
+Commit: Fix memory leak in event handlers
