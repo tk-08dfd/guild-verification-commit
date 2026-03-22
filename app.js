4c7d5917ad4ec381b3ef994afa823b8a32b1d842
tk-08dfd <nguyenphuyenkhang@icloud.com>
2026-03-22 19:58:29 +0700

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-22T12:58:10.636Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-22T12:58:28.425Z
+Commit: Resolve timezone issues in date handling
